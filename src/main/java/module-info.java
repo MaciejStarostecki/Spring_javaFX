a545c95c3f27d53457e51a9d035a8bc4882625a2
Maciej Starostecki <maciejstarostecki@gmail.com>
2023-11-17 18:20:34 +0100

    Commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,6 @@ module pl.strefakursow.spring_javafx {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
 
     opens pl.strefakursow.spring_javafx to javafx.fxml;
     exports pl.strefakursow.spring_javafx;
